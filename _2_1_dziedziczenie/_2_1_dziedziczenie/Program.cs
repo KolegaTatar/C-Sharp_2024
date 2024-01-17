ba9edc628563e50e416fde4380655d59dbfce319
Wiktor <wiktor.tatarynowicz@gmail.com>
2024-01-17 07:57:43 +0100

    dziedziczenie

---
+++ _2_1_dziedziczenie/_2_1_dziedziczenie/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _2_1_dziedziczenie
+{
+    internal class Program
+    {
+        class Person
+        {
+            public string Name { get; set; };
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
