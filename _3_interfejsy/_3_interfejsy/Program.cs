d187e4de5bc8116ae60ad3a0f777b3a8e2c3579a
Wiktor <wiktor.tatarynowicz@gmail.com>
2024-01-31 09:29:17 +0100

    add lesson 3_interfejsy

---
+++ _3_interfejsy/_3_interfejsy/Program.cs
@@ -0,0 +1,66 @@
+﻿using _3_interfejsy.classes;
+
+namespace _3_interfejsy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Book> books = new List<Book>();
+
+            books.Add(new Book("Hobbit1", "Nowak", 1937, 45.99));
+            books.Add(new Book("Hobbit2", "Pawlak", 2000, 145.99));
+            books.Add(new Book("Hobbit3", "Arbuz", 2000, 5.99));
+            books.Add(new Book("Hobbit4", "Arbuz", 1948, 45.99));
+
+
+            Console.WriteLine("Lista książek: ");
+            foreach (Book book in books)
+            {
+                Console.WriteLine(book);
+            }
+            
+
+            Console.WriteLine("\nPosortowane książki według ceny");
+            books.Sort();
+            foreach (Book book in books)
+            {
+                Console.WriteLine(book);
+            }
+
+
+            Console.WriteLine("\nPosortowane książki według daty publikacji");
+            var shortedByYear = books.OrderBy(e =>e.YearOfPublication).ToList();
+            foreach (Book book in shortedByYear)
+            {
+                Console.WriteLine(book);
+            }
+
+            Console.WriteLine("\nPosortowane książki według daty publikacji od końca");
+            var shortedByYear_end_to_first = books.OrderByDescending(e => e.YearOfPublication).ToList();
+            foreach (Book book in shortedByYear_end_to_first)
+            {
+                Console.WriteLine(book);
+            }
+
+
+
+            Console.WriteLine("\nPosortowane książki według autor publikacji");
+            var shortedByAuthor = books.OrderBy(e => e.Author).ToList();
+            foreach (Book book in shortedByAuthor)
+            {
+                Console.WriteLine(book);
+            }
+
+            Console.WriteLine("\nPosortowane książki według ceny nierosnąco, a następnie według roku publikacji od najstarszej ksiązki:");
+            var shortedByPriceAndYeaar = books.OrderByDescending(e => e.Price).ThenBy(e =>e.YearOfPublication);
+            foreach (Book book in shortedByPriceAndYeaar)
+            {
+                Console.WriteLine(book);
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ _3_interfejsy/_3_interfejsy/classes/Book.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_interfejsy.classes
+{
+    internal class Book: IComparable<Book>
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        public ushort YearOfPublication { get; set; }
+        public double Price { get; set; }
+
+        public Book(string title, string author, ushort year, double price)
+        {
+            Title = title;
+            Author = author;
+            YearOfPublication = year;
+            Price = price;
+        }
+
+        public int CompareTo(Book? other)
+        {
+            if(other == null) return 1; // musi być int bo zwraca 0,1 lub -1
+            return Price.CompareTo(other.Price);
+        }
+
+        
+        public override string ToString()
+        {
+            return $"{Title}, {Author}, {YearOfPublication}, {Price}zł";
+           
+        }
+
+   
+
+    }
+}
